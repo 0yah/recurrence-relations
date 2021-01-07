12b325d5e5385641e5bf671965f3d62f9f692ba7
0yah <highmeme10@gmail.com>
2021-01-07 18:16:01 +0300

    Factorial

--- factorial.py
+++ factorial.py
@@ -1,1 +1,11 @@
 #Factorial
+"""
+The product of all positive integers less than or equal to n
+"""
+def factorial(n):
+    if n == 0:
+        return 1
+    return n*factorial(n-1)
+
+result = factorial(10)
+print(result)
